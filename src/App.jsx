819f79b8e3f41e46d42d53c4745a246803162866
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2025-09-17 08:15:26 +0600

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import {Route, Routes} from "react-router-dom"
-import Home from "./pages/home"
-import Favorites from "./pages/favorites"
-import Details from "./pages/details"
-import Navbar from "./components/navbar"
 
-export default function App() {
-  return (
-    <div>
-      <div className="min-h-screen p-6 bg-white text-grey-600 text-lg">
-        <Navbar />
-        <div>Test components</div>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/favorites" element={<Favorites/>}/>
-          <Route path="/recipe-item/:id" element={<Details/>}/>
-        </Routes>
-      </div>
-    </div>
-  )
+import './App.css'
+
+function App() {
+  return <div>Bro</div>
 }
+
+export default App
